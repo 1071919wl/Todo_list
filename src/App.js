3e3c357cf4a0650ae757fd0d5c5623257ae34541
William Leung <Wllm@Williams-MacBook-Pro.local>
2020-05-06 18:29:51 -0700

    sharing my todo list file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList'
+
+const Local_Storage_Key = 'todoApp.todos'
 
 function App() {
+  const[todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+
+//START- SAVES DATA EVEN WHEN PAGE IS REFRESHED
+useEffect(()=>{
+  const storedTodos = JSON.parse(localStorage.getItem(Local_Storage_Key))
+  if (storedTodos) setTodos(storedTodos)
+},[])
+
+useEffect(()=>{
+  localStorage.setItem(Local_Storage_Key, JSON.stringify(todos))
+}, [todos])
+//END- SAVES DATA EVEN WHEN PAGE IS REFRESHED
+
+
+// START- CHECK BOX FOR TOGGLING
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+//END- CHECK BOX FOR TOGGLING
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    const random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    if (name === '') return
+    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: random, name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function handleClearTodos(){
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <TodoList todos={todos} toggleTodo={toggleTodo} />
+    <input ref={todoNameRef} type ='text' />
+    <button onClick={handleAddTodo}>Add Todo</button>
+    <button onClick={handleClearTodos}>Clear Completed Todos</button>
+    <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+  </div>
+  )
 }
+
 
 export default App;
